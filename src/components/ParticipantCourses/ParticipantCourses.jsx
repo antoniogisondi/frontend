7e1add0855ebb7bac191aff3925da74cc2e74057
Antonio Gisondi <anto.gisondi@tiscali.it>
2024-12-13 12:48:20 +0100

    fix

--- src/components/ParticipantCourses/ParticipantCourses.jsx
+++ src/components/ParticipantCourses/ParticipantCourses.jsx
@@ -1,17 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParticipantAuth } from '../../context/ParticipantAuthContext';
-import { createPayment } from '../../services/participantService';
-import DownloadCertificate from '../DownloadCertificate/DownloadCertificate';
 import { Link } from 'react-router-dom';
-import { loadStripe } from '@stripe/stripe-js';
 import Loader from '../Loader/Loader';
 import './ParticipantCourses.css';
 
 function ParticipantCourses() {
-    const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
     const { participantDetails } = useParticipantAuth();
     const [courses, setCourses] = useState([]);
-    const [paidCourses, setPaidCourses] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const participantCourses = participantDetails.courseId;
@@ -20,16 +15,6 @@ function ParticipantCourses() {
         try {
             if (participantCourses && participantDetails) {
                 setCourses(participantCourses);
-
-                // Calcola i corsi pagati
-                const paidCoursesList = participantCourses.map(course => {
-                    const isPaid = participantDetails.payments.some(payment => 
-                        payment.courseId === course._id && payment.status === 'succeeded'
-                    );
-                    return { courseId: course._id, isPaid };
-                });
-
-                setPaidCourses(paidCoursesList);
             } else {
                 setError('Non ci sono corsi associati a questo partecipante');
             }
@@ -39,19 +24,6 @@ function ParticipantCourses() {
             setLoading(false);
         }
     }, [participantCourses, participantDetails]);
-
-    const handlePayment = async (participantId, courseId, nome_corso, autorizzazione, costo) => {
-        try {
-            const stripe = await stripePromise;
-            const response = await createPayment(participantId, courseId, nome_corso, autorizzazione, costo);
-            stripe.redirectToCheckout({ sessionId: response.id });
-        } catch (error) {
-            setError('Errore durante la creazione della sessione di pagamento.');
-            console.error(error);
-        } finally {
-            setLoading(false);
-        }
-    };
 
     if (loading) return <div className='container-fluid gx-0'><Loader /></div>;
 
@@ -63,50 +35,29 @@ function ParticipantCourses() {
                 <p className="text-center text-muted">Non sei ancora iscritto a nessun corso.</p>
             ) : (
                 <div className="row">
-                    {courses.map((course) => {
-                        const isPaid = paidCourses.find(p => p.courseId === course._id)?.isPaid;
-
-                        return (
-                            <div className="col-md-4 mb-4" key={course._id}>
-                                <div className="card shadow-sm course-card">
-                                    <div className="card-body">
-                                        <h5 className="card-title">{course.nome_corso}</h5>
-                                        <p className="card-text">
-                                            <strong>Categoria:</strong> {course.categoria_corso}<br />
-                                            <strong>Stato:</strong> {course.status}<br />
-                                            <strong>Durata Totale:</strong> {Array.isArray(course.durata_corso) && course.durata_corso.length > 0
-                                                ? course.durata_corso.reduce((acc, curr) => acc + curr.durata_ore, 0)
-                                                : 'N/A'
-                                            } ore
-                                        </p>
-                                        <Link className="btn btn-primary btn-sm w-100" to={`/participant-dashboard/corsi/${course._id}`}>
-                                            Visualizza i dettagli
-                                        </Link>
-                                        {course.status === 'Completato' && isPaid ? (
-                                            <div>
-                                                <DownloadCertificate courseId={course._id} participantId={participantDetails._id} />
-                                            </div>
-                                        ) : course.status === 'Completato' && !isPaid ? (
-                                            <button
-                                                className="btn btn-primary w-100"
-                                                onClick={() => handlePayment(participantDetails._id, course._id, course.nome_corso, course.numero_autorizzazione, course.costo)}
-                                            >
-                                                Effettua il pagamento
-                                            </button>
-                                        ) : course.status === 'Attivo' ? (
-                                            <p>Il corso è iniziato</p>
-                                        ) : (
-                                            <p>Il corso deve ancora iniziare</p>
-                                        )}
-                                    </div>
+                    {courses.map((course) => (
+                        <div className="col-md-4 mb-4" key={course._id}>
+                            <div className="card shadow-sm course-card">
+                                <div className="card-body">
+                                    <h5 className="card-title">{course.nome_corso}</h5>
+                                    <p className="card-text">
+                                        <strong>Categoria:</strong> {course.categoria_corso}<br />
+                                        <strong>Stato:</strong> {course.status}<br />
+                                        <strong>Durata Totale:</strong> {Array.isArray(course.durata_corso) && course.durata_corso.length > 0
+                                            ? course.durata_corso.reduce((acc, curr) => acc + curr.durata_ore, 0)
+                                            : 'N/A'
+                                        } ore
+                                    </p>
+                                    <Link className="btn btn-primary btn-sm w-100" to={`/participant-dashboard/corsi/${course._id}`}>
+                                        Visualizza i dettagli
+                                    </Link>
                                 </div>
                             </div>
-                        );
-                    })}
+                        </div>
+                    ))}
                 </div>
             )}
         </div>
     );
 }
-
 export default ParticipantCourses;
--- src/components/ParticipantCoursesDetails/ParticipantCoursesDetails.jsx
+++ src/components/ParticipantCoursesDetails/ParticipantCoursesDetails.jsx
@@ -1,12 +1,17 @@
 import React, {useEffect, useState} from 'react'
+import DownloadCertificate from '../DownloadCertificate/DownloadCertificate';
+import Loader from '../Loader/Loader'
 import { useParams } from 'react-router-dom'
 import { useParticipantAuth } from '../../context/ParticipantAuthContext'
-import Loader from '../Loader/Loader'
+import { createPayment } from '../../services/participantService';
+import { loadStripe } from '@stripe/stripe-js';
 
 function ParticipantCoursesDetails() {
+    const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
     const {id} = useParams()
     const {participantDetails} = useParticipantAuth()
     const [course, setCourse] = useState(null)
+    const [isPaid, setIsPaid] = useState(false);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const participantCourses = participantDetails.courseId
@@ -20,16 +25,42 @@ function ParticipantCoursesDetails() {
                 } else {
                     setError('Errore nel recupero dei dettagli del corso')
                 }
+                const paidId = specificCourse._id
+
+                const isPaidCourse = participantDetails.payments.some(payment => 
+                    payment.courseId === paidId && payment.status === 'succeeded'
+                );
+
+                if(isPaidCourse){
+                    setIsPaid(isPaidCourse)
+                } else {
+                    setError('Nessun corso risulta pagato')
+                }
             } else {
                 setError('Errore nel recupero dei dettagli del corso')
             }
+            
+            
         } catch (error) {
             setError(error.message || 'Errore durante il recupero dei dettagli del corso')
         } finally {
             setLoading(false)
         }
 
-    }, [id, participantCourses])
+    }, [id, participantCourses, participantDetails])
+
+    const handlePayment = async (participantId, courseId, nome_corso, autorizzazione, costo) => {
+        try {
+            const stripe = await stripePromise;
+            const response = await createPayment(participantId, courseId, nome_corso, autorizzazione, costo);
+            stripe.redirectToCheckout({ sessionId: response.id });
+        } catch (error) {
+            setError('Errore durante la creazione della sessione di pagamento.');
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     if (loading) return <div className='container-fluid gx-0'><Loader/></div>
     
@@ -41,6 +72,17 @@ function ParticipantCoursesDetails() {
                 <div className="row">
                     <div className="col">
                         <h1>{course.nome_corso}</h1>
+                        {course.status === 'Completato' && isPaid ? (
+                            <DownloadCertificate courseId={course._id} participantId={participantDetails._id} />
+                        ) : course.status === 'Completato' ? (
+                        <button className="btn btn-primary w-100" onClick={() => handlePayment(participantDetails._id, course._id, course.nome_corso, course.numero_autorizzazione, course.costo)}>
+                            Effettua il pagamento
+                        </button>
+                        ) :  course.status === 'Attivo' ?(
+                            <p>Il corso è attivo</p>
+                        ) : (
+                            <p>Il corso è stato richiesto</p>
+                        )}
                     </div>
                 </div>
             )}
